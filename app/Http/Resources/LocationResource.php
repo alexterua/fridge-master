b7f83185521e07a4862880ee22a97e141a7da72e
alexterua <alexter.od.ua@gmail.com>
2022-08-02 11:30:30 +0300

    Added to LocationResource count of free blocks

--- app/Http/Resources/LocationResource.php
+++ app/Http/Resources/LocationResource.php
@@ -16,6 +16,21 @@ class LocationResource extends JsonResource
     {
         return [
             'name' => $this->name,
+            'countOfFreeBlocks' => $this->getCountOfFreeBlocks($this),
         ];
+    }
+
+    private function getCountOfFreeBlocks(LocationResource $resource) : int
+    {
+        $countOfFreeBlocks = 0;
+        $premises = $resource->premises;
+        foreach ($premises as $premis) {
+            $blocks = $premis->blocks;
+            foreach ($blocks as $block) {
+                if ($block->is_free === 1)
+                    $countOfFreeBlocks++;
+            }
+        }
+        return $countOfFreeBlocks;
     }
 }
